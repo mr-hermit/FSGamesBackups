8051eee0a515602d8607ab129324011c4ba05ea7
Ilya Zolotko <ilya.zolotko@yahoo.com>
2023-07-14 11:01:43 -0400

    Update restore-ER.ps1
    Script automatically identify path for current Windows user APPDATA folder

--- restore-ER.ps1
+++ restore-ER.ps1
@@ -1,3 +1,7 @@
-$LastBackup = Get-ChildItem C:\Users\zolotko\Desktop\EldenRing\ -File | Sort-Object CreationTime -Descending | Select-Object -First 1
-Expand-Archive -Path $LastBackup.FullName -DestinationPath C:\Users\zolotko\AppData\Roaming\EldenRing\ -Force
+$BackupPath = C:\EldenRingBackups
+$LastBackup = Get-ChildItem $BackupPath -File | Sort-Object CreationTime -Descending | Select-Object -First 1
+
+$userappdata = [Environment]::GetFolderPath('ApplicationData')
+
+Expand-Archive -Path $LastBackup.FullName -DestinationPath $userappdata\ -Force
 Start-Sleep -Seconds 5
